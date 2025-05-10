e7d2bc8cd4bfd6881743b76dc01ece10bd978ef6
Sparsha <129596934+sparshabhusal@users.noreply.github.com>
2025-05-10 08:23:02 +0545

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo pacman -S --needed base-devel git
 sudo git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+cd ~
 yay -S google-chrome
 cd ~
 mkdir .themes
@@ -14,15 +15,19 @@ cd Downloads
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1
 cd WhiteSur-gtk-theme
 sudo ./install.sh
+cd ~
  cd Downloads
  git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
  cd WhiteSur-icon-theme
  sudo ./install.sh
+ cd ~
  cd Downloads
  git clone https://github.com/vinceliuice/WhiteSur-cursors.git
  cd WhiteSur-cursors
  sudo ./install.sh
+ cd ~
   cd Downloads
  git clone https://github.com/vinceliuice/grub2-themes.git
  cd grub2-themes
  sudo ./install.sh
+ cd ~
